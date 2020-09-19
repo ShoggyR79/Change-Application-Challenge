fd2cb50f44b538935462dbed77f3ed8aa39b6a47
Du Duong <khaidu2002@gmail.com>
2020-09-18 22:59:06 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # please replace entities in quotes with your own API.
 
 
-api_key = "J1vCFHVRQsxUDwUrylGfkdiPO"
-api_secret = "Xi0ZswCiZvTgYHYCyo7ucGK8qytLpLEmfxQzc2BLI6UKSLugAc"
-access_token = "794730854354448384-g7aORg91y3e6m6jkLJwFqpVyrUNkpXE"
-token_secret = "9O4MJnJZRJ4fTX8k2zYjG0CHUwpA3KFDeEoNXYkcThofI"
+api_key = "apikey"
+api_secret = "secret"
+access_token = "token"
+token_secret = "token_secret"
